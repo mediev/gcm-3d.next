159acb64dfa4afe965042c0bb252302fa4106cce
Alexey Vasyukov <vasyukov@gmail.com>
2014-11-29 16:45:00 +0300

    POC3: - Нет шаблонов - Один базовый тип нода (CalcNode) - Теоретическая возможность добавить новый тип нода ценой своей реализации сетки

---
+++ src/poc3/nodes.cpp
@@ -0,0 +1,5 @@
+#include "nodes.hpp"
+
+int CalcNode::VALUES_NUMBER = -1;
+int MyNewCalcNode::VALUES_NUMBER = -1;
+
---
+++ src/poc3/nodes.hpp
@@ -0,0 +1,26 @@
+#ifndef NODES
+#define NODES 1
+
+// Base class
+class Node
+{
+public:
+};
+
+// Default calc node
+class CalcNode : public Node
+{
+public:
+	static int VALUES_NUMBER;
+	float values[10];
+};
+
+// Completely custom calc node
+class MyNewCalcNode : public CalcNode
+{
+public:
+	static int VALUES_NUMBER;
+	float values[21];
+};
+
+#endif
